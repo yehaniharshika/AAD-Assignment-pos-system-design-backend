77c5f3e7891a2a627bc7c019b5603d382ba84e05
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-25 18:13:07 +0530

    create doPost method save item success

--- src/main/java/lk/ijse/possystem/controller/ItemController.java
+++ src/main/java/lk/ijse/possystem/controller/ItemController.java
@@ -1,14 +1,22 @@
 package lk.ijse.possystem.controller;
 
+import jakarta.json.JsonException;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.possystem.dao.impl.ItemDAOImpl;
+import lk.ijse.possystem.dto.ItemDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -22,12 +30,48 @@ public class ItemController extends HttpServlet {
         logger.info("initializing ItemController with call init method");
         try {
             var ctx = new InitialContext();
-            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/pos");
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/posSystem");
             this.connection = pool.getConnection();
 
         } catch (SQLException |NamingException e){
             logger.error("init method loading failed");
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.debug("call doPost method");
+
+        try(var writer = resp.getWriter()){
+            Jsonb jsonb = JsonbBuilder.create();
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(),ItemDTO.class);
+
+            var itemDAOImpl = new ItemDAOImpl();
+            if (itemDAOImpl.saveItem(itemDTO,connection)){
+                writer.write("Item saved successfully");
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            }else {
+                writer.write("Failed to save item");
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        }catch (JsonException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
