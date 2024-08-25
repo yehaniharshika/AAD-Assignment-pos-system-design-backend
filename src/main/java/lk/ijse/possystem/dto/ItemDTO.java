874cc3b979a2e79b11810f21dca4f372c472815f
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-25 17:03:53 +0530

    create ItemDTO

--- src/main/java/lk/ijse/possystem/dto/ItemDTO.java
+++ src/main/java/lk/ijse/possystem/dto/ItemDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.possystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ItemDTO {
+    private String  itemCode;
+    private String  itemName;
+    private String  unitPrice;
+    private String  qtyOnHand;
 }
