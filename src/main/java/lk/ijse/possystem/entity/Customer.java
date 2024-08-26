0318f6334939fb98833ff128de54a9279b398f8c
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-26 15:49:23 +0530

    create Customer entity

--- src/main/java/lk/ijse/possystem/entity/Customer.java
+++ src/main/java/lk/ijse/possystem/entity/Customer.java
@@ -1,4 +1,16 @@
 package lk.ijse.possystem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Customer {
+    private String customerId;
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String email;
 }
