2c6178ab22467c4ad3b88e300d3754f87f03edc4
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-26 18:19:31 +0530

    create CrudDAO

--- src/main/java/lk/ijse/possystem/dao/CrudDAO.java
+++ src/main/java/lk/ijse/possystem/dao/CrudDAO.java
@@ -1,5 +1,20 @@
 package lk.ijse.possystem.dao;
 
-public interface CrudDAO {
+import lk.ijse.possystem.dto.CustomerDTO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudDAO<T> extends SuperDAO {
+
+    boolean save(T entity, Connection connection) throws SQLException;
+
+    boolean update(String id, T entity, Connection connection) throws SQLException;
+
+    boolean delete(String id, Connection connection) throws SQLException;
+
+    List<T> getAll(Connection connection) throws SQLException;
+
+    T getById(String id, Connection connection) throws SQLException;
 }
