0f45d6e1a02ed23f8dc32d96b4a61a380d65d007
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-26 18:21:25 +0530

    create CustomerBO interface with methods

--- src/main/java/lk/ijse/possystem/bo/CustomerBO.java
+++ src/main/java/lk/ijse/possystem/bo/CustomerBO.java
@@ -1,4 +1,15 @@
 package lk.ijse.possystem.bo;
 
+import lk.ijse.possystem.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface CustomerBO {
+    boolean saveCustomer(CustomerDTO customerDTO, Connection connection) throws SQLException;
+    boolean updateCustomer(String customerId,CustomerDTO customerDTO,Connection connection) throws SQLException;
+    boolean deleteCustomer(String customerId,Connection connection) throws SQLException;
+    List<CustomerDTO> getCustomers(Connection connection) throws SQLException;
+    CustomerDTO getCustomerByCustomerId(String customerId,Connection connection) throws SQLException;
 }
--- src/main/java/lk/ijse/possystem/bo/ItemBO.java
+++ src/main/java/lk/ijse/possystem/bo/ItemBO.java
@@ -1,4 +1,5 @@
 package lk.ijse.possystem.bo;
 
 public interface ItemBO {
+
 }
