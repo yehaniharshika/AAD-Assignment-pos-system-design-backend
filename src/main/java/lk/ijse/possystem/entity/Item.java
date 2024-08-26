c9098b38d396fdcac56f8a626115d757ab9e3f84
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-08-26 19:34:07 +0530

    implement Item entity

--- src/main/java/lk/ijse/possystem/entity/Item.java
+++ src/main/java/lk/ijse/possystem/entity/Item.java
@@ -1,4 +1,15 @@
 package lk.ijse.possystem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Item {
+    private String  itemCode;
+    private String  itemName;
+    private String  unitPrice;
+    private String  qtyOnHand;
 }
